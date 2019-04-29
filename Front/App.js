21a207889e3792f6bb44221e8e7e029782d1acc5
lucarl <carl.e.w.lundborg@gmail.com>
2019-04-29 18:53:30 +0200

    added simple hello world

--- Front/App.js
+++ Front/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MainContent from './components/MainContent'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <MainContent/>
     );
   }
 }
